cb4294d3be5ada6410da642f9706ab9bff554879
Caio Leonhardt <caioleonhardt@MacBook-Air-de-Caio.local>
2019-05-05 23:42:41 -0300

    fix truffle network

--- Project_5_Starter_Code/truffle-config.js
+++ Project_5_Starter_Code/truffle-config.js
@@ -75,7 +75,7 @@ module.exports = {
 
     // Useful for private networks
     rinkeby: {
-      provider: () => new HDWalletProvider(mnemonic, `rinkeby.infura.io/v3/{infuraKey}`),
+      provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/${infuraKey}`),
       network_id: 4,       // rinkeby's id
       gas: 4500000,        // rinkeby has a lower block limit than mainnet
       gasPrice: 10000000000
